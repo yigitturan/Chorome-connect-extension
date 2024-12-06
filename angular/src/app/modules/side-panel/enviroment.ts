26d0fe92545601ee49d344ab208bccb744d14b86
Yigit Turan <yit2@calvin.edu>
2024-12-05 21:11:19 -0500

    signin with mail

---
+++ angular/src/app/modules/side-panel/enviroment.ts
@@ -0,0 +1,14 @@
+// environment.ts
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyAAW_VUOWqlP_PX0pn9ADM6NR4umUN10UI",
+        authDomain: "extension-bf8bc.firebaseapp.com",
+        projectId: "extension-bf8bc",
+        storageBucket: "extension-bf8bc.firebasestorage.app",
+        messagingSenderId: "66780522120",
+        appId: "1:66780522120:web:fe578457c092d4d17d613d",
+        measurementId: "G-03JR6D1JJT"
+    },
+  };
+  
